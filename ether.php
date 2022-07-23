dccaa751b7b0399df66e6599269b8e5d3631aabf
javedboqo <javedboqo@gmail.com>
2022-07-23 16:33:13 +0500

    Added JSON file

--- ether.php
+++ ether.php
@@ -1,55 +1,46 @@
 <html>
-    <head>NFT</head>
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    </head>
     <body>
-        <h1>Call NFT with Ether for Ticketly</h1>
+        <div class="text-center"><h2>Call NFT with Ether for Ticketly</h2></div>
 <div class="container">
-  <center>
-  <h2>Upload to IPFS</h2>
-  <input id="fileUpload" type="file" class="btn btn-primary">Select File</input>
-  <br /><br />
-  <div id="ipfs_hash"></div>
-  </center>
-</div>
-
-        <a href="javascript:mintMeta()">
-            Mint NFT</a>
+        <form>
+        <div style="display: flex;flex-direction: column;">      
+            <label class="form-label">Upload to IPFS</label>
+            <input type="file" accept="image/*" name="image" id="file" class="form-control">
+        </div>
+        </form>
+        <div style="display: flex;flex-direction: column;">      
+          <a href="javascript:init()" class="btn btn-primary">Connect to Etherium BlockChain</a>
+          <a href="javascript:mintMeta()" class="btn btn-success" style="margin-top: 10px;">Mint NFT</a>
+        </div>
+        </div>
     </body>
 </html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/ipfs-http-client/dist/index.min.js"></script>
 <script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" type="application/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/@biconomy/mexa@latest/dist/mexa.js"></script>
 
 <script>
 
-
-function uploadToIPFS() {
-    if(!window.IpfsHttpClient) return;
-    var ipfs = window.IpfsHttpClient({ host: 'ipfs.infura.io', port: 5001 });
-    document.getElementById("ipfs_hash").innerHTML = "<b>Uploading...Please wait</b>"
-    
-    var reader = new FileReader();
-    reader.onload = function() {
-
-        var arrayBuffer = this.result,
-        array = new Uint8Array(arrayBuffer),
-        binaryString = String.fromCharCode.apply(null, array);
-
-        //console.log(result)
-        ipfs.add(binaryString, (err, result) => {
-        console.log(result)
-        document.getElementById("ipfs_hash").innerHTML = "<b>IPFS Hash of uploded file: </b>"+result[0].hash 
-        })
-    }
-    reader.readAsArrayBuffer(this.files[0]);
+document.querySelector('#file').addEventListener('change', upload);
+async function upload() {
+      var fileReader = new FileReader()
+      // Read file as ArrayBuffer
+      await fileReader.readAsArrayBuffer(event.target.files[0])
+      //  Listen for the onload event
+      fileReader.onload = async (event) => {            
+          //const node = await Ipfs.create({ host: 'ipfs.infura.io', port: 5001,protocol:'https' })
+          var node = await Ipfs.create({url:'https://ipfs.infura.io'});
+          // upload the file content
+          var { path } = await node.add(fileReader.result)
+          
+          console.log(path)
+      }//*/
 }
-
-    
-    document.querySelector('#fileUpload').addEventListener('change', function() {
-        uploadToIPFS();
-    }, false);
-
-
 </script>
 
 <script>
@@ -57,412 +48,14 @@ function uploadToIPFS() {
     var nftContractAddress = "0x417a3863b1aae2F3bdDF37a22D5e24A248C9b539";
     var contract;
     var biconomy;
-    var abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_trustedForwarder",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "approved",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "Approval",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "bool",
-          "name": "approved",
-          "type": "bool"
-        }
-      ],
-      "name": "ApprovalForAll",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "Transfer",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "approve",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        }
-      ],
-      "name": "balanceOf",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "getApproved",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        }
-      ],
-      "name": "isApprovedForAll",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "forwarder",
-          "type": "address"
-        }
-      ],
-      "name": "isTrustedForwarder",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [],
-      "name": "name",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "ownerOf",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "safeTransferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bytes",
-          "name": "_data",
-          "type": "bytes"
-        }
-      ],
-      "name": "safeTransferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        },
-        {
-          "internalType": "bool",
-          "name": "approved",
-          "type": "bool"
-        }
-      ],
-      "name": "setApprovalForAll",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "bytes4",
-          "name": "interfaceId",
-          "type": "bytes4"
-        }
-      ],
-      "name": "supportsInterface",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [],
-      "name": "symbol",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [],
-      "name": "tokenCount",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "_value",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "tokenURI",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "transferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_tokenURI",
-          "type": "string"
-        }
-      ],
-      "name": "mint",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ];
+    
     async function init () {
     if (typeof window.ethereum !== "undefined") {
+
+      $.getJSON('NFT.json', async function(nftJson) {
+        console.log(nftJson.abi);
+        var abi  =nftJson.abi;
+
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
@@ -481,8 +74,7 @@ function uploadToIPFS() {
         }
       );
       console.log('Biconomy1',biconomy);
-      
-      
+            
       biconomy
         .onEvent(biconomy.READY, async () => {
             console.log("ready biconomy")
@@ -501,7 +93,7 @@ function uploadToIPFS() {
           console.log("biconomy.ERROR 1", message);
           console.log("biconomy.ERROR 2", error);
         });//*/
-      
+      });
     } else {
       console.log("Wallet not found");
       alert("please enable wallet");
@@ -562,9 +154,5 @@ function uploadToIPFS() {
       }
     } catch (error) {}
   }
-
-  $(function(){
-     //init();
-  });
 </script>
 
